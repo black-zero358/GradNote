f830dd73ed27b9ad4bec8bb67d3ad971f8150cdc
black_zero <blackzero35800@gmail.com>
2026-01-27 12:29:16 +0800

    feat: 添加本地CORS源以支持开发环境

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -71,6 +71,8 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://localhost:8000",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:8000",
     ]
 
     # 日志配置
